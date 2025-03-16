5810ed06a01181c0bc5fdca1e7a7b02a32311154
Jolinamata <jolinamata707@gmail.com>
2025-03-16 15:21:58 -0800

    Updates

--- assets/status-tap-legacy-K8-s_Ubn.js
+++ assets/status-tap-legacy-K8-s_Ubn.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-D0qla4WQ.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.a,o=e.w,i=e.s}],execute:function(){
+System.register(["./index-legacy-u__pDjWu.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.r,r=e.f,s=e.a,o=e.w,i=e.s}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
