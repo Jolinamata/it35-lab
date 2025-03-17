a953d8af5c45b67a0dd918a09c1b782bbf606db1
jolinamata <jolinamata707@gmail.com>
2025-03-17 10:23:27 -0800

    Updates

--- assets/swipe-back-legacy-COwvxDUv.js
+++ assets/swipe-back-legacy-COwvxDUv.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-C8aGLzgF.js","./index3-legacy-DF-BMsfn.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
+System.register(["./index-legacy-3lIrw8xu.js","./index3-legacy-DFCQhpas.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
