5473b68e87f58fc6cb6379376e13a8be9fb43d02
Jolinamata <jolinamata707@gmail.com>
2025-03-25 23:15:20 -0800

    Updates

--- assets/ios.transition-legacy-BtdsXD7Y.js
+++ assets/ios.transition-legacy-BtdsXD7Y.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DN1aBQ-K.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
+System.register(["./index-legacy-b8clPHnN.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/keyboard2-legacy-_t8lEhbO.js
+++ assets/keyboard2-legacy-_t8lEhbO.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DN1aBQ-K.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
+System.register(["./index-legacy-b8clPHnN.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
