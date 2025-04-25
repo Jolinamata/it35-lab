206f88f3b13d4fbed881a624a6597a479c9ab6bd
Jolinamata <jolinamata707@gmail.com>
2025-04-26 00:10:30 +0800

    Modified Menu.tsx and added the profile button

--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -14,16 +14,18 @@ import {
       IonTitle, 
       IonToolbar 
   } from '@ionic/react'
-  import {homeOutline, logOutOutline, rocketOutline} from 'ionicons/icons';
+  import {homeOutline, logOutOutline, rocketOutline, settingsOutline} from 'ionicons/icons';
 import { Redirect, Route } from 'react-router';
 import Home from './Home';
 import About from './About';
 import Details from './Details';
+import Profile from './EditProfilePage';
 
   const Menu: React.FC = () => {
     const path = [
         {name:'Home', url: '/it35-lab/app/home', icon: homeOutline},
         {name:'About', url: '/it35-lab/app/about', icon: rocketOutline},
+        {name:'Profile', url: '/it35-lab/app/profile', icon: settingsOutline},
     ]
 
     return (
@@ -60,6 +62,7 @@ import Details from './Details';
                     <Route exact path="/it35-lab/app/home" component={Home} />
                     <Route exact path="/it35-lab/app/home/details" component={Details} />
                     <Route exact path="/it35-lab/app/about" component={About} />
+                    <Route exact path="/it35-lab/app/profile" component={Profile} />
 
                     <Route exact path="/it35-lab/app">
                         <Redirect to="/it35-lab/app/home"/>
