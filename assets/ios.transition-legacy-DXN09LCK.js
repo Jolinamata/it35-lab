7b85d0debada29c777f75508f27df9f43a43f2e5
Jolinamata <jolinamata707@gmail.com>
2025-04-25 19:57:26 +0800

    Updates

--- assets/ios.transition-legacy-DXN09LCK.js
+++ assets/ios.transition-legacy-DXN09LCK.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-MvwXrU1u.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-OqJs8GjC.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
