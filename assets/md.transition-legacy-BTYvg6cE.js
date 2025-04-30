64f3b88a3f1609687f7a8ae3e603ff52b342e643
jolinamata <jolinamata707@gmail.com>
2025-04-29 23:39:59 -0800

    Updates

--- assets/md.transition-legacy-BTYvg6cE.js
+++ assets/md.transition-legacy-BTYvg6cE.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DuyistrQ.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-BJBEomEh.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
